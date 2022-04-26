33e93d8a25392e2d9a75287cc1420347d5895d9b
beeno24 <77907170+beeno24@users.noreply.github.com>
2022-04-26 10:23:13 -0500

    Update sketch_apr19a.ino

--- sketch_apr19a/sketch_apr19a.ino
+++ sketch_apr19a/sketch_apr19a.ino
@@ -1,60 +1,19 @@
-/*
-    WiFi Connect - connecting Arduino Nano 33 BLE to WiFi using an ESP-01 and
-    WiFiEspAT library. WiFi settings stored in seperate configuration file. 
-*/
-#include <WiFiEspAT.h>    // Include the Wi-Fi library
-#include "config.h"
-
-char ssid[] = "EBEN";        // your network SSID (name)
-char password[] = "1234";    
-
+char inputByte;
 void setup() {
-
-    // Start the Serial communication to send messages to the computer
-    Serial.begin(115200);
-    while(!Serial);
-    delay(10);
-    Serial.println("Arduino Nano 33 BLE WiFi Connection Demo");
-
-    // Start serial communication to wifi module
-    Serial1.begin(115200);
-    WiFi.init(Serial1);
-
-    // check for the WiFi module:
-    if (WiFi.status() == WL_NO_MODULE) {
-        Serial.println();
-        Serial.println("Communication with WiFi module failed!");
-        // don't continue
-        while (true);
-    }
-
-    WiFi.endAP(true); // to disable default automatic start of persistent AP at startup
-
-    WiFi.setPersistent(); // set the following WiFi connection as persistent
-
-
-    // Connect to the WiFi network
-    WiFi.begin(ssid, password);
-    Serial.print("Connecting to SSID: ");
-    Serial.print(ssid); Serial.println(" ...");
-
-    while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
-        delay(1000);
-        Serial.print('.');
-    }
-
-    Serial.println("");
-    Serial.println("Connection established!");
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());         // Get the assigned IP address
-
-    // print the received signal strength:
-    Serial.print("signal strength (RSSI):");
-    Serial.print(WiFi.RSSI());
-    Serial.println(" dBm");
+ Serial.begin(9600);
+ pinMode(LED_BUILTIN,OUTPUT);
 
 }
 
 void loop() {
-    // empty
+while(Serial.available()>0){
+  inputByte= Serial.read();
+  Serial.println(inputByte);
+  if (inputByte=='Z'){
+  digitalWrite(LED_BUILTIN,HIGH);
+  }
+  else if (inputByte=='z'){
+  digitalWrite(LED_BUILTIN,LOW);
+  } 
+  }
 }
